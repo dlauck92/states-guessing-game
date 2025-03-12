bf3ce4dfd82b585253c9bedba1cf0364d67aaf73
Drew Lauck <drewlauck92@gmail.com>
2025-03-12 12:45:12 -0400

    removing commented out logic

--- main.py
+++ main.py
@@ -16,9 +16,6 @@ while len(guessed_states) < 50:
     
     if answer_state is None:  # User clicked "Cancel"
         missing_states = [state for state in all_states if state not in guessed_states]
-        # for state in all_states:
-        #     if state not in guessed_states:
-        #         missing_states.append(state)
         new_data = pandas.DataFrame(missing_states)
         new_data.to_csv("states_to_learn.csv")
         break  # Exit the loop
